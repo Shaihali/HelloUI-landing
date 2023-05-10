9136bba6b6071a930cc51a080ad3c6b97cefb9da
Шайхали Сайбулаев <saybulaevshm@DAGBTI.LOCAL>
2023-05-10 12:46:58 +0300

    refactor: add and config  webpack-config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: {
+		index: './index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{ test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css'
+		})
+	],
+	mode: 'development',
+};
